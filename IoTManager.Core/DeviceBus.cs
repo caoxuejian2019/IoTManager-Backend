f56559bf7faf379d11cfb5cc8d54c519b9f16226
Rampb <1159578029@qq.com>
2019-04-28 08:57:20 +0000

    Redesign Architecture

---
+++ IoTManager.Core/DeviceBus.cs
@@ -0,0 +1,11 @@
+﻿using IoTManager.Core.Infrastructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IoTManager.Core
+{
+    public sealed class DeviceBus:IDeviceBus
+    {
+    }
+}
---
+++ IoTManager.Model/DepartmentModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IoTManager.Model
+{
+    public sealed class DepartmentModel
+    {
+    }
+}
