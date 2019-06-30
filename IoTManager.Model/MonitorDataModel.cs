4725c0f2eff1b4e8db3be151a623c7ab1cc85fea
Rampb <1159578029@qq.com>
2019-06-30 05:29:35 +0000

    Add Local Monitor Data Sync App

---
+++ IoTManager.Model/MonitorDataModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IoTManager.Model
+{
+    public class MonitorDataModel
+    {
+        public string GatewayId { get; set; }
+        public string DeviceId { get; set; }
+        public string TypeName { get; set; }
+        public string Type { get; set; }
+        public string Unit { get; set; }
+        public string Value { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ IoTManager.Tcp.MonitorDataSync/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IoTManager.Tcp.MonitorDataSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
